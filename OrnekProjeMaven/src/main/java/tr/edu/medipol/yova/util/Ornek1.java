b757e53c7c9ed6f34ea87eb93bb4f8ba271b039d
yigitoner28 <yigitoner28@gmail.com>
2023-05-02 21:15:10 +0300

    Maven proje ornek

---
+++ OrnekProjeMaven/src/main/java/tr/edu/medipol/yova/util/Ornek1.java
@@ -0,0 +1,17 @@
+package tr.edu.medipol.yova.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Ornek1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(StringUtils.deleteWhitespace("   ab   c   "));
+		
+		
+		
+
+	}
+
+}
